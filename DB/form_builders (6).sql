277aadc5557c802a90a96cc6b47e17785b5577a9
Darshil122 <darshildabhi2005@gmail.com>
2024-08-08 23:45:06 +0530

    Added databse

---
+++ DB/form_builders (6).sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2024 at 08:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `form_builders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formfield_master`
+--
+
+CREATE TABLE `formfield_master` (
+  `id` int(11) NOT NULL,
+  `form_id` int(11) DEFAULT NULL,
+  `field_name` varchar(255) DEFAULT NULL,
+  `field_type` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `formfield_master`
+--
+
+INSERT INTO `formfield_master` (`id`, `form_id`, `field_name`, `field_type`, `created_at`) VALUES
+(1, 1, 'Text Field', 'text', '2024-08-08 03:51:09'),
+(2, 2, 'Number Field', 'number', '2024-08-08 04:01:31'),
+(3, 3, 'Email Field', 'email', '2024-08-08 04:38:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forms_master`
+--
+
+CREATE TABLE `forms_master` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `form_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `forms_master`
+--
+
+INSERT INTO `forms_master` (`id`, `user_id`, `form_name`, `created_at`) VALUES
+(1, 3, 'jay form', '2024-08-08 03:51:09'),
+(2, 3, 'jay second form', '2024-08-08 04:01:31'),
+(3, 2, 'jaydev form', '2024-08-08 04:38:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_master`
+--
+
+CREATE TABLE `login_master` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_master`
+--
+
+INSERT INTO `login_master` (`id`, `email`, `Password`) VALUES
+(1, 'admin@gmail.com', '2005');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_master`
+--
+
+CREATE TABLE `user_master` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_master`
+--
+
+INSERT INTO `user_master` (`id`, `email`, `password`) VALUES
+(1, 'darshil@gmail.com', '2000'),
+(2, 'jaydev@gmail.com', '1000'),
+(3, 'jay@gmail.com', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `formfield_master`
+--
+ALTER TABLE `formfield_master`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `form_id` (`form_id`);
+
+--
+-- Indexes for table `forms_master`
+--
+ALTER TABLE `forms_master`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `forms_master_fk1` (`user_id`);
+
+--
+-- Indexes for table `login_master`
+--
+ALTER TABLE `login_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_master`
+--
+ALTER TABLE `user_master`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `formfield_master`
+--
+ALTER TABLE `formfield_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `forms_master`
+--
+ALTER TABLE `forms_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `login_master`
+--
+ALTER TABLE `login_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_master`
+--
+ALTER TABLE `user_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `formfield_master`
+--
+ALTER TABLE `formfield_master`
+  ADD CONSTRAINT `formfield_master_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `forms_master` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `forms_master`
+--
+ALTER TABLE `forms_master`
+  ADD CONSTRAINT `forms_master_fk1` FOREIGN KEY (`user_id`) REFERENCES `user_master` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
