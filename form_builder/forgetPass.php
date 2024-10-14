b271b8dbee13bf49ac71de8ffd1624db8e06fc15
Darshil122 <darshildabhi2005@gmail.com>
2024-10-14 22:46:04 +0530

    Add File

---
+++ form_builder/forgetPass.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/jpg" href="dist/img/navicon.png">
+    <?php include "./Include/style.php"; ?>
+    <title>Foreget Password</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1 class="h404">404</h1>
+            <h1 class="page">Page Not Found</h1>
+            <img src="dist/img/404.png" alt="Page Not Found" class="img404">
+            <a href="login.php" class="btn btn-danger Goback">Go Back</a>
+        </div>
+    </div>
+</body>
+<?php include "./Include/script.php"; ?>
+
+</html>
